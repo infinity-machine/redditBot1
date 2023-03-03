e91bbfd804abe008fb2a146fb0cf2142dc485eba
Connor Carciofini <ccarciofini@gmail.com>
2023-03-03 14:58:07 -0600

    parsing data

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 import os
 import praw
 # import random
+import pprint
 
 from dotenv import load_dotenv
 load_dotenv()
 
 from gpt import respondToComment
+from utils import getProperties
 
 reddit = praw.Reddit(
     client_id=os.getenv('CLIENT_ID'),
@@ -15,9 +17,11 @@ reddit = praw.Reddit(
     password=os.getenv('PASSWORD')
 )
 
-# subreddit = reddit.subreddit('KitchenConfidential')
+subreddit = reddit.subreddit('explainlikeimfive')
 
-# my_response = 'Congrats dude!'
+# for post in subreddit.new(limit=1):
+#     pprint.pprint(getProperties(post))
+
 
 # for post in subreddit.new(limit=1):
 #     print("---post---\n")
@@ -25,7 +29,7 @@ reddit = praw.Reddit(
 #     print("Text: ", post.selftext)
 #     print("Score: ", post.score)
 #     print("---post---\n")
-#     # post.reply(my_response)
+#     post.reply(my_response)
     
 #     for comment in post.comments:
 #         if hasattr(comment, 'body'):
@@ -33,5 +37,12 @@ reddit = praw.Reddit(
 #             print(comment.body)
 #             print("---comment---\n")
 
-print(respondToComment('hello'))
+# for post in subreddit.new(limit=1):
+#     print(post.title)
+#     post.reply(respondToComment(post.title))
+
+
+
+
+
 
