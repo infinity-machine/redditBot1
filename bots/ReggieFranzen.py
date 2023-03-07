6f2c8eb805cb1ac840c4ef64c89785c0f39bfbc7
Connor Carciofini <ccarciofini@gmail.com>
2023-03-07 11:19:41 -0600

    trimmed trash

--- bots/ReggieFranzen.py
+++ bots/ReggieFranzen.py
@@ -3,6 +3,7 @@ import praw
 import openai
 import pprint
 import random
+import time
 
 from gpt import gptResponse
 
@@ -17,86 +18,30 @@ reddit_rf = praw.Reddit(
     password=os.getenv('RF_PASSWORD')
 )
 
-# subreddit_ELIF = reddit_rf.subreddit('explainlikeimfive')
+def likeXSubredditPosts(subreddits_array, number_of):
+    selected = random.choices(subreddits_array, k=number_of)
+    for subreddit in selected:
+        print(f'IN SUBREDDIT "{subreddit}"')
+        for post in subreddit.new(limit=1):
+            # post.upvote()
+            print(f'REGGIE FRANZEN HAS LIKED "{post.title}"')
+            print('...')
+            time.sleep(3)
+
+def getSubreddits():
+    list = []
+    for this_subreddit in reddit_rf.user.subreddits(limit=None):
+        list.append(this_subreddit)
+    return list 
 
 
-# def upvoteRecentSubredditPost(a_subreddit):
-#     subreddit = reddit_rf.subreddit(a_subreddit)
-#     for post in subreddit.new(limit=1):
-#         # post.upvote()
-#         print(f"REGGIE FRANZEN HAS UPVOTED POST '{post.title}'IN SUBREDDIT '{subreddit}'")
 
 
 
-
-
-
-
-
-
-
-
-# def chooseRandomSelections(subreddit_list):
-#     # GET ONE TO THREE USER SUBREDDITS
-#     for x in range(random.randint(1, 3)):
-#         this_subreddit = subreddit_list[random.randint(0, (len(subreddit_list) - 1))]
-#         print(this_subreddit)
-#         # upvoteRecentSubredditPost(this_subreddit)
-
-
-# def getSubreddits(reddit_instance):
-#     list = []
-#     for this_subreddit in reddit_instance.user.subreddits(limit=None):
-#         list.append(this_subreddit)
-#     return sorted(list)
-
-# def upvoteSomePosts():
-#     subreddits = getSubreddits(reddit_rf)
-#     print(subreddits)
-#     # selections = chooseRandomSelections(subreddits)
 
 def ReggieFranzen():
-    # LIST OF SUBREDDITS ON USER
-    # upvoteSomePosts()
-    for subreddit in reddit_rf.user.subreddits():
-        print(subreddit)
-    # print(reddit_rf.user.subreddits())
-
-
+    subreddits = getSubreddits()
+    likeXSubredditPosts(subreddits, 3)
+    # time.sleep(300)
 
 ReggieFranzen()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## BETTER WAY TO DO THIS?
-## OUT OF THE 10 NEWEST REDDIT POSTS, PICK THE FIRST THAT DOES NOT HAVE A BODY PROPERTY AND RETURN
-# def grabELIFPost():
-#     for post in subreddit_ELIF.new(limit=10):
-#         if post.selftext == '':
-#             return post
-
-
-# openai.api_key = os.getenv('OPENAI_API_KEY')
-
-# def gptResponse(input):
-#     completion = openai.Completion.create(
-#         model="text-davinci-003",
-#         prompt=input,
-#         max_tokens=200,
-#         temperature=1
-#     )
-#     output = completion.choices[0].text.strip('\n\n')
-#     return output
-
-# pprint.pprint(dir(reddit_rf))
