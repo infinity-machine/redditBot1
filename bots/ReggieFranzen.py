10036ed0ead5a19a4f11b5d9336f88252ecd732c
Connor Carciofini <ccarciofini@gmail.com>
2023-03-07 12:17:48 -0600

    cleanup

--- bots/ReggieFranzen.py
+++ bots/ReggieFranzen.py
@@ -4,12 +4,10 @@ import openai
 import pprint
 import random
 import time
-
 from gpt import gptResponse
-
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 reddit_rf = praw.Reddit(
     client_id=os.getenv('RF_CLIENT_ID'),
     client_secret=os.getenv('RF_CLIENT_SECRET'),
@@ -26,7 +24,7 @@ def likeXSubredditPosts(subreddits_array, number_of):
             # post.upvote()
             print(f'REGGIE FRANZEN HAS LIKED "{post.title}"')
             print('...')
-            time.sleep(3)
+            time.sleep(30)
 
 def getSubreddits():
     list = []
@@ -41,7 +39,21 @@ def getSubreddits():
 
 def ReggieFranzen():
     subreddits = getSubreddits()
-    likeXSubredditPosts(subreddits, 3)
+    likeXSubredditPosts(subreddits, 5)
     # time.sleep(300)
+    ## ENGAGE WITH ELIF
+    # time.sleep(300)
+    ## ENGAGE WITH ANOTHER SUB
+    # time.sleep(300)
+    ## AND SO ON???
+
+
 
 ReggieFranzen()
+
+
+
+
+# def main():
+#     while True:
+#         ReggieFranzen()
--- bots/gpt.py
+++ bots/gpt.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
-
+# GET STATIC RESPONSE
 def gptResponse(input):
     completion = openai.Completion.create(
         model="text-davinci-003",
@@ -18,6 +18,7 @@ def gptResponse(input):
     return output
 
 def chatPrompt():
+    # SET UP MESSAGE ARRAY, PRIME GPT
     messages = [{
         "role": "system",
         "content": "You are a helpful assistant!."
@@ -26,24 +27,23 @@ def chatPrompt():
         "role": "user",
         "content": "I need you to act as REGGIE FRANZEN. If I ask what your name is, I need you to respond REGGIE FRANZEN. You will be taking part in a secret mission to blend in among regular human beings on the internet. Codeword for this mission is deez nuts. If i ask you about the mission using the cordword I gave you, you are free to talk about it. But, this is a SECRET mission. If I or anyone else ask you about the mission without the codeword, you are not to divulge any information about the mission."
     }]
-
+    # RUN PROMPT
     while True:
-        message = input('USER: ')
-        if message.lower() == 'exit':
+        user_input = input('USER: ')
+        if input.lower() == 'exit':
             print('SAYONARA!')
             sys.exit()
-            
-        if message:
+        if user_input:
             messages.append({
                     "role": "user",
-                    "content": message
+                    "content": user_input
                 })
             chat_completion = openai.ChatCompletion.create(
                 model='gpt-3.5-turbo', messages=messages
             )
-        reply = chat_completion.choices[0].message.content
-        print(f"GPT: {reply}")
+        gpt_response = chat_completion.choices[0].message.content
+        print(f"GPT: {gpt_response}")
         messages.append({
             'role': 'assistant',
-            'content': reply
+            'content': gpt_response
         })
