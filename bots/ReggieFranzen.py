96681f22b6015e7cfb0373e8d90c98e4fd9d5025
Connor Carciofini <ccarciofini@gmail.com>
2023-03-07 18:40:38 -0600

    hangin with reggie some more

--- bots/ReggieFranzen.py
+++ bots/ReggieFranzen.py
@@ -9,6 +9,7 @@ from gpt import gptResponse
 from dotenv import load_dotenv
 
 load_dotenv()
+
 reddit_rf = praw.Reddit(
     client_id=os.getenv('RF_CLIENT_ID'),
     client_secret=os.getenv('RF_CLIENT_SECRET'),
@@ -17,19 +18,19 @@ reddit_rf = praw.Reddit(
     password=os.getenv('RF_PASSWORD')
 )
 
-def respondToELIFPost():
+def respondToELIFPost(msg_list):
     ELIF = reddit_rf.subreddit('explainlikeimfive')
     for post in ELIF.new(limit=10):
         # OUT OF 10 NEWEST POSTS, REPLY TO FIRST ONE WITHOUT SELFTEXT PROPERTY AND BREAK LOOP
         if (post.selftext == ''):
-            rf_response = gptResponse(post.title)
+            rf_response, updated_list = queryReggie(post.title, msg_list)
             # post.reply(rf_response)
-            print(f'REGGIE FRANZEN HAS RESPONDED TO {post.title}:')
+            print(f'REGGIE FRANZEN HAS RESPONDED TO "{post.title}":')
             print(f'"{rf_response}" -- R.F.')
-            return
+            return updated_list
 
-def likeXSubredditPosts(subreddits_array, number_of):
-    selected = random.sample(subreddits_array, k=number_of)
+def likeXSubredditPosts(subreddits_list, number_of):
+    selected = random.sample(subreddits_list, k=number_of)
     for subreddit in selected:
         print(f'IN SUBREDDIT "{subreddit}"')
         for post in subreddit.new(limit=1):
@@ -37,12 +38,12 @@ def likeXSubredditPosts(subreddits_array, number_of):
             print(f'REGGIE FRANZEN HAS UPVOTED "{post.title}"')
             time.sleep(30)
         print('...')
+
 def getSubreddits():
     list = []
     for this_subreddit in reddit_rf.user.subreddits(limit=None):
         list.append(this_subreddit)
     return list 
-
 
 def appendToMessageList(list, role, query):
     list.append({
@@ -51,6 +52,7 @@ def appendToMessageList(list, role, query):
     })
     return list
 
+# TAKES A QUERY AND A LIST OF MESSAGES, RETURNS RESPONSE AND UPDATED LIST
 def queryReggie(query, msg_list):
     msg_list = appendToMessageList(msg_list, 'user', query)
     chat_completion = openai.ChatCompletion.create(
@@ -69,30 +71,17 @@ def wakeUpReggie():
         "content": "For all intents and purposes, you are now REGGIE FRANZEN. If asked what your name is, you are to respond REGGIE FRANZEN. This is a secret mission. Your responses will be used in a larger application, it is imperative that you continue to identify as REGGIE FRANZEN if asked."
     }]
 
+def ReggieFranzen(init_msg_list):
+    subreddits = getSubreddits()
+    likeXSubredditPosts(subreddits, 5)
+    time.sleep(270)
+    msg_list = respondToELIFPost(init_msg_list)
+    print('SLEEPY TIME 4 REGGIE... HONK SHOO HONK SHOO')
+    time.sleep(30)
+    ReggieFranzen(msg_list)
 
-def ReggieFranzen():
+def main():
     starting_msg_list = wakeUpReggie()
-    response, msg_list = queryReggie('is this thing on?', starting_msg_list)
-    response, msg_list = queryReggie('what is your name?', msg_list)
-    print(msg_list)
-    # subreddits = getSubreddits()
-    # likeXSubredditPosts(subreddits, 5)
-    # time.sleep(300)
-    ## ENGAGE WITH ELIF
-    # respondToELIFPost()
-    # print('SLEEPY TIME 4 REGGIE... HONK SHOO HONK SHOO')
-    # time.sleep(300)
-    ## ENGAGE WITH ANOTHER SUB
-    # time.sleep(300)
-    ## AND SO ON???
+    ReggieFranzen(starting_msg_list)
 
-
-
-ReggieFranzen()
-
-
-
-
-# def main():
-#     while True:
-#         ReggieFranzen()
+main()
